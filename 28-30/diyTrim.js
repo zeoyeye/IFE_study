307befd863898c4831eda2714021a2fe069ad88a
zeoyeye <573810495@qq.com>
2018-06-06 20:46:14 +0800

    Add files via upload

---
+++ 28-30/diyTrim.js
@@ -0,0 +1,12 @@
+﻿function diyTrim(str) {
+	for(var i=0;i<str.length;i++){
+		if(str[i]!=" "&&str[i]!="　")
+			break;
+	}
+	var result = str.substring(i);
+	for(var j=str.length-1;j>-1;j--){
+		if(str[j]!=" "&&str[j]!="　")
+			break;
+	}
+	return result.substring(0,j+1);
+}
