95bf31e7873e9dc9ee919f1d168e5dc02789d5e2
zeoyeye <573810495@qq.com>
2018-06-15 22:54:04 +0800

    Delete app.js

--- 31-33/app.js
+++
@@ -1,8 +0,0 @@
-﻿var regionContainer = document.getElementById("container1");
-var regionCheckbox = document.getElementsByName("region");
-var productContainer = document.getElementById("container2");
-var productCheckbox = document.getElementsByName("product");
-var div1 = document.getElementById("region-radio-wrapper");
-
-checkboxClick(regionContainer,regionCheckbox);
-checkboxClick(productContainer,productCheckbox);
