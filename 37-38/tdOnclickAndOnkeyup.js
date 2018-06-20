1178ac94e5e35394cf710b339a44618e985953b2
zeoyeye <573810495@qq.com>
2018-06-20 23:52:29 +0800

    Add files via upload

---
+++ 37-38/tdOnclickAndOnkeyup.js
@@ -0,0 +1,24 @@
+﻿div1.onclick = function(event){
+	if(event.target.tagName=="TD"){			//点击单元格，保存单元格数据到localStorage
+		event.target.innerHTML = parseInt(event.target.innerHTML);
+		localStorage.setItem("textValue",event.target.innerHTML);
+		toText(event);					//再将单元格转换成input
+	}
+	if(event.target.value=="保存"){
+		makeSure(event);				//修改单元格数据为输入的值
+	}
+	else if(event.target.value=="取消"){	//点取消，单元格恢复成原数据
+		var tdNode = event.target.parentNode;
+		tdNode.innerHTML = localStorage.textValue;
+	}
+}
+div1.onkeyup = function(event){
+	if(event.target.type=="text"){
+		if(event.keyCode==27){
+			cancel(event);
+		}else if(event.keyCode==13){
+			makeSure(event);
+		}
+
+	}
+}
---
+++ 37-38/tdToText.js
@@ -0,0 +1,29 @@
+﻿function cancel(event){			//输入框的blur事件和取消按钮的click事件调用
+	event.target.parentNode.innerHTML = localStorage.textValue;
+}
+function makeSure(event){		//输入框的keyup事件和保存按钮的click事件调用
+	var textNode  = event.target.parentNode.firstChild;
+	if(isNaN(Number(textNode.value))){
+		alert("输入不合法！");
+	}else{
+		localStorage.textValue = textNode.value;
+		event.target.parentNode.innerHTML = localStorage.textValue;
+	}	
+}
+function toText(event){			//单元格innerHTML改为空，再添加3个input子节点
+	var tdValue = event.target.innerHTML;
+	event.target.innerHTML = "";
+	var input = new Array(3);
+	var type = ["text","button","button"];
+	var value = [tdValue,"取消","保存"];
+	for(var i=0;i<input.length;i++){
+		input[i]  = document.createElement("input");
+		input[i].type = type[i];
+		input[i].value = value[i];
+		input[i].name = "inputs";
+		event.target.appendChild(input[i]);
+	}
+	input[0].focus();			//并使焦点位于输入框，设置输入框的blur事件处理程序
+	input[0].setAttribute("onblur","cancel(event)");
+}
+
