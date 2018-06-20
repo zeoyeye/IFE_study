236a1aeac62d834d5d6079d5c5181899cc17f58f
zeoyeye <573810495@qq.com>
2018-06-20 23:06:02 +0800

    Update app.js

--- 31-33/app.js
+++ 31-33/app.js
@@ -6,3 +6,12 @@ var div1 = document.getElementById("dataTable");
 
 checkboxClick(regionContainer,regionCheckbox);
 checkboxClick(productContainer,productCheckbox);
+
+window.onload = function(){
+	for(var i=0,len=regionCheckbox.length;i<len;i++){
+		regionCheckbox[i].checked = "checked";
+		productCheckbox[i].checked = "checked";
+	}
+		div1.innerHTML = getNewTable1(sourceData);	
+		combineTD()；
+}
